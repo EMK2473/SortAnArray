cc24bd769a77fdd5d8f77997f339ed452e184d90
Eric Keeton <142048130+EMK2473@users.noreply.github.com>
2024-09-13 15:49:26 -0400

    imported java util Arrays for sort method, defined sortArray method sorting myArr in ascending order; defined input length and assigned input unt to array elements; looped through and printed for testing

--- SortAnArray.java
+++ SortAnArray.java
@@ -1,12 +1,28 @@
 package SortAnArray;
 
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class SortAnArray {
 
    /* Define your method here */
+   public static void sortArray(int[] myArr, int arrSize){
+    Arrays.sort(myArr);
+   }
 
    public static void main(String[] args) {
       /* Type your code here. */
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_VALUES = scnr.nextInt();
+      System.out.println("NUM_VALUES: " + NUM_VALUES);
+      int[] myArray = new int[NUM_VALUES];
+      
+      for(int i = 0; i < NUM_VALUES; ++i) {
+         myArray[i] = scnr.nextInt();
+         System.out.println(myArray[i] + ", ");
+      }
+      
+      sortArray(myArray, NUM_VALUES);
+      scnr.close();
    }
 }
